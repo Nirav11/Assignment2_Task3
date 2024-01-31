b4fa8ce95b69a0a6e67876cfc92ce94a437d751b
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-01-31 04:00:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,75 @@
-      require(["esri/Map", "esri/layers/CSVLayer", "esri/views/MapView", "esri/widgets/Legend"], (
-        Map,
-        CSVLayer,
-        MapView,
-        Legend
-      ) => {
-        var url = "https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%202/stl_crime_wgs_84.csv";";
+require(["esri/Map", "esri/layers/CSVLayer", "esri/views/MapView", "esri/widgets/Legend"], (
+    Map,
+    CSVLayer,
+    MapView,
+    Legend
+  ) => {
+    var url = "https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%202/stl_crime_wgs_84.csv";
 
-        // Paste the url into a browser's address bar to download and view the attributes
-        // in the CSV file. These attributes include:
-        // * mag - magnitude
-        // * type - earthquake or other event such as nuclear test
-        // * place - location of the event
-        // * time - the time of the event
+    // Paste the url into a browser's address bar to download and view the attributes
+    // in the CSV file. These attributes include:
+    // * mag - magnitude
+    // * type - earthquake or other event such as nuclear test
+    // * place - location of the event
+    // * time - the time of the event
 
-        const template = {
-          title: "Crime committed at {ILEADSStreet}"
-        };
+    const template = {
+      title: "Crime committed at {ILEADSStreet}"    
+    };
 
-        // The heatmap renderer assigns each pixel in the view with
-        // an intensity value. The ratio of that intensity value
-        // to the maxPixel intensity is used to assign a color
-        // from the continuous color ramp in the colorStops property
+    // The heatmap renderer assigns each pixel in the view with
+    // an intensity value. The ratio of that intensity value
+    // to the maxPixel intensity is used to assign a color
+    // from the continuous color ramp in the colorStops property
 
-        const renderer = {
-          type: "heatmap",
-          colorStops: [
-            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
-            { color: "#472b77", ratio: 0.083 },
-            { color: "#4e2d87", ratio: 0.166 },
-            { color: "#563098", ratio: 0.249 },
-            { color: "#5d32a8", ratio: 0.332 },
-            { color: "#6735be", ratio: 0.415 },
-            { color: "#7139d4", ratio: 0.498 },
-            { color: "#7b3ce9", ratio: 0.581 },
-            { color: "#853fff", ratio: 0.664 },
-            { color: "#a46fbf", ratio: 0.747 },
-            { color: "#c29f80", ratio: 0.83 },
-            { color: "#e0cf40", ratio: 0.913 },
-            { color: "#ffff00", ratio: 1 }
-          ],
-          maxDensity: 0.01,
-          minDensity: 0
-        };
+    const renderer = {
+      type: "heatmap",
+      colorStops: [
+        { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+        { color: "#472b77", ratio: 0.083 },
+        { color: "#4e2d87", ratio: 0.166 },
+        { color: "#563098", ratio: 0.249 },
+        { color: "#5d32a8", ratio: 0.332 },
+        { color: "#6735be", ratio: 0.415 },
+        { color: "#7139d4", ratio: 0.498 },
+        { color: "#7b3ce9", ratio: 0.581 },
+        { color: "#853fff", ratio: 0.664 },
+        { color: "#a46fbf", ratio: 0.747 },
+        { color: "#c29f80", ratio: 0.83 },
+        { color: "#e0cf40", ratio: 0.913 },
+        { color: "#ffff00", ratio: 1 }
+      ],
+      maxDensity: 0.01,
+      minDensity: 0
+    };
 
-      const layer = new CSVLayer({
+
+    const layer = new CSVLayer({
         url: url,
         title: "St. Louis Crime Heatmap",
         copyright: "St. Louis Police Department",
-		latitudeField:"Lat",
-        longitudeField:"Lon",
+		latitudeField:"Latitude",
+        longitudeField:"Longitude",
 		popupTemplate: template,
 		renderer: renderer
-});
+}); 
+ 
+    const map = new Map({
+      basemap: "gray-vector",
+      layers: [layer]
+    });
 
+    const view = new MapView({
+      container: "viewDiv",
+      center: [-90.1994, 38.6270],
+      zoom: 2,
+      map: map
+    });
 
-        const map = new Map({
-          basemap: "gray-vector",
-          layers: [layer]
-        });
-
-        const view = new MapView({
-          container: "viewDiv",
-          center: [-138, 30],
-          zoom: 2,
-          map: map
-        });
-
-        view.ui.add(
-          new Legend({
-            view: view
-          }),
-          "bottom-left"
-        );
-      });
-    </script>
+    view.ui.add(
+      new Legend({
+        view: view
+      }),
+      "bottom-left"
+    );
+  });
